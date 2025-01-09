a165f52cf317f0f9e66c4f87a01ca463b7931f03
johnnysn <johnnysn@gmail.com>
2025-01-09 15:33:08 -0300

    feat: Make task collapsible

--- src/lib/components/TaskItem.svelte
+++ src/lib/components/TaskItem.svelte
@@ -5,7 +5,7 @@
 	import { type Task } from '$lib/types/task';
 	import { cn } from '$lib/utils';
 	import Button from './ui/button/button.svelte';
-	import { Trash2, Edit } from 'lucide-svelte';
+	import { Trash2, ChevronsDown, ChevronsUp } from 'lucide-svelte';
 
 	type Props = {
 		task: Task;
@@ -17,12 +17,14 @@
 	let nameInput: HTMLInputElement;
 	let nameValue = $state(task.name);
 	let descriptionValue = $state(task.description);
+	let editMode = $state(false);
 
 	const labelClass = $derived(`${task.done ? 'line-through' : ''}`);
 
 	$effect(() => {
 		if (task.id === tasks.empty) {
 			nameInput.focus();
+			editMode = true;
 		}
 	});
 
@@ -45,39 +47,64 @@
 </script>
 
 <div
-	class="items-top flex w-full max-w-screen-sm space-x-2 rounded border border-transparent px-1 py-2 hover:border-border"
+	class="flex w-full max-w-screen-sm flex-col rounded border border-transparent bg-background/40 px-2 py-2 hover:border-border"
 >
-	<Checkbox {id} onCheckedChange={(checked) => updateDone(!!checked)} />
-	<div class="grid flex-1 items-center gap-1.5 leading-none">
-		<Label
-			for={id}
-			class={`text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${labelClass}`}
-		>
+	<div class="flex w-full items-center space-x-2">
+		<Checkbox
+			{id}
+			onCheckedChange={(checked) => updateDone(!!checked)}
+			checked={task.done}
+			size={'lg'}
+		/>
+		<div class="grid flex-1 items-center gap-1.5 leading-none">
+			<Label
+				for={id}
+				class={`text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${labelClass}`}
+			>
+				<input
+					type="text"
+					class={cn(
+						'w-[180px] border-none bg-transparent p-0 text-foreground focus:text-foreground focus:outline-none focus:ring-0',
+						labelClass
+					)}
+					onblur={updateTask}
+					bind:value={nameValue}
+					bind:this={nameInput}
+				/>
+			</Label>
+		</div>
+		<div class="items-top flex w-[80px] gap-1">
+			<Button
+				variant="outline"
+				size="icon"
+				on:click={() => {
+					editMode = !editMode;
+				}}
+			>
+				{#if editMode}
+					<ChevronsUp class="size-4" />
+				{:else}
+					<ChevronsDown class="size-4" />
+				{/if}
+			</Button>
+			<Button variant="outline" size="icon" on:click={del}>
+				<Trash2 class="size-4" />
+			</Button>
+		</div>
+	</div>
+	{#if editMode}
+		<div class="flex flex-col pl-3">
+			<div class="flex items-center gap-2">
+				<Checkbox id={id + 'rec'} size={'sm'} />
+				<Label for={id + 'rec'} class="text-sm">Recurrent</Label>
+			</div>
 			<input
 				type="text"
-				class={cn(
-					'w-[180px] border-none bg-transparent p-0 text-foreground focus:text-foreground focus:outline-none focus:ring-0',
-					labelClass
-				)}
+				class="w-[180px] border-none bg-transparent p-0 text-sm text-foreground/50 focus:text-foreground focus:outline-none focus:ring-0"
+				placeholder="Add description here"
 				onblur={updateTask}
-				bind:value={nameValue}
-				bind:this={nameInput}
+				bind:value={descriptionValue}
 			/>
-		</Label>
-		<input
-			type="text"
-			class="w-[180px] border-none bg-transparent p-0 text-sm text-foreground/50 focus:text-foreground focus:outline-none focus:ring-0"
-			placeholder="Add description"
-			onblur={updateTask}
-			bind:value={descriptionValue}
-		/>
-	</div>
-	<div class="items-top flex w-[80px] gap-1">
-		<Button variant="outline" size="icon" on:click={del}>
-			<Edit class="size-4" />
-		</Button>
-		<Button variant="outline" size="icon" on:click={del}>
-			<Trash2 class="size-4" />
-		</Button>
-	</div>
+		</div>
+	{/if}
 </div>
--- src/lib/components/ui/checkbox/checkbox.svelte
+++ src/lib/components/ui/checkbox/checkbox.svelte
@@ -4,17 +4,34 @@
 	import Minus from 'lucide-svelte/icons/minus';
 	import { cn } from '$lib/utils.js';
 
-	type $$Props = CheckboxPrimitive.Props;
+	type $$Props = CheckboxPrimitive.Props & { size?: 'md' | 'sm' | 'lg' };
 	type $$Events = CheckboxPrimitive.Events;
 
 	let className: $$Props['class'] = undefined;
 	export let checked: $$Props['checked'] = false;
+	export let size: $$Props['size'] = 'md';
 	export { className as class };
+
+	let size1Class = 'h-4 w-4';
+	let size2Class = 'h-3.5 w-3.5';
+	$: {
+		if (size === 'lg') {
+			size1Class = 'h-6 w-6';
+			size2Class = 'h-5.5 w-5.5';
+		} else if (size === 'sm') {
+			size1Class = 'h-3 w-3';
+			size2Class = 'h-2.5 w-2.5';
+		} else {
+			size1Class = 'h-4 w-4';
+			size2Class = 'h-3.5 w-3.5';
+		}
+	}
 </script>
 
 <CheckboxPrimitive.Root
 	class={cn(
-		'border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer box-content h-6 w-6 shrink-0 rounded-sm border focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-50',
+		'peer box-content shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[disabled=true]:cursor-not-allowed data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[disabled=true]:opacity-50',
+		size1Class,
 		className
 	)}
 	bind:checked
@@ -22,14 +39,14 @@
 	on:click
 >
 	<CheckboxPrimitive.Indicator
-		class={cn('flex h-6 w-6 items-center justify-center text-current')}
+		class={cn('flex items-center justify-center text-current', size1Class)}
 		let:isChecked
 		let:isIndeterminate
 	>
 		{#if isChecked}
-			<Check class="h-5.5 w-5.5" />
+			<Check class={size2Class} />
 		{:else if isIndeterminate}
-			<Minus class="h-5.5 w-5.5" />
+			<Minus class={size2Class} />
 		{/if}
 	</CheckboxPrimitive.Indicator>
 </CheckboxPrimitive.Root>

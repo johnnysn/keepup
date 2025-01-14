5120f60dcac04e6ea258f92b4ad7515679e2b657
johnnysn <johnnysn@gmail.com>
2025-01-14 09:44:28 -0300

    feat: Add task at the end

--- src/lib/components/TaskList.svelte
+++ src/lib/components/TaskList.svelte
@@ -3,10 +3,11 @@
 	import { dragHandleZone, dragHandle, type DndEvent } from 'svelte-dnd-action';
 	import type { Task } from '$lib/types/task';
 	import { formatDate } from '$lib/utils';
-	import { tasks, updateDailyArrayOrder } from '$lib/store/tasks-store.svelte';
+	import { addNewTaskNow, tasks, updateDailyArrayOrder } from '$lib/store/tasks-store.svelte';
 	import TaskItem from './TaskItem.svelte';
-	import { GripVertical } from 'lucide-svelte';
-	import { blur } from 'svelte/transition';
+	import { GripVertical, Plus } from 'lucide-svelte';
+	// import { blur } from 'svelte/transition';
+	import Button from './ui/button/button.svelte';
 
 	let items = $state<Task[]>([]);
 
@@ -23,20 +24,23 @@
 		items = e.detail.items;
 	}
 	function handleDndFinalize(e: CustomEvent<DndEvent<Task>>) {
+		items = e.detail.items;
 		updateDailyArrayOrder(e.detail.items);
+	}
+	function addTask() {
+		addNewTaskNow(items.length);
 	}
 </script>
 
 <ul
-	use:dragHandleZone={{ items, flipDurationMs }}
+	use:dragHandleZone={{ items, flipDurationMs, dragDisabled: true }}
 	onconsider={handleDndConsider}
 	onfinalize={handleDndFinalize}
-	class="flex flex-col items-center gap-2 py-8"
+	class="flex flex-col items-center gap-2 pb-2 pt-8"
 >
 	{#each items as item (item.id)}
 		<li
 			animate:flip={{ duration: flipDurationMs }}
-			transition:blur
 			class="flex w-full max-w-screen-sm items-center rounded border border-transparent bg-background/40 hover:border-border"
 		>
 			<div use:dragHandle class="mx-1 text-foreground/50 hover:text-foreground">
@@ -46,3 +50,12 @@
 		</li>
 	{/each}
 </ul>
+
+{#if items.length > 0}
+	<div class="flex w-full justify-center text-foreground/50 hover:text-foreground">
+		<Button variant="ghost" class="flex items-center gap-1.5" onclick={addTask}>
+			<Plus />
+			<span>Add task</span>
+		</Button>
+	</div>
+{/if}
